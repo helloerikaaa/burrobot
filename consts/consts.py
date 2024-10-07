56ee9134fd8754b64eebb55dcf5f2487c08468b5
Erika Sánchez-Femat <erikasafe@gmail.com>
2024-10-07 16:30:33 -0600

    [ADD] Python version and dependencies
    [Author] Erika Sánchez-Femat
    
    -- Added pyproject for dependencies
    -- Added .python-version to lock python version of this project

---
+++ consts/consts.py
@@ -0,0 +1,14 @@
+class Colors:
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    GREEN = (0, 255, 0)
+    GRAY = (128, 128, 128)
+
+
+class MazeConf:
+    SCREEN_WIDTH = 850
+    SCREEN_HEIGHT = 850
+    MAZE_WIDTH = 10
+    MAZE_HEIGHT = 10
+    GOAL_X = 9
+    GOAL_Y = 0
